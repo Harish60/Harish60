11f64648d93009d7f21d6d433cb5f97121c8fb59
Harish60 <68767531+Harish60@users.noreply.github.com>
2022-04-30 18:53:06 +0530

    Update and rename README.md to sorts.java

--- sorts.java
+++ sorts.java
@@ -1,38 +1,4 @@
-/*
-Given a list of letters and a dictionary containing a list of valid words, generate all arrangements of words from the given letters.
-Input Format
-First line is a String s, containing the letters you can arrange.
-Second line is the number of words (integer n) in the dictionary.
-The next n lines contains the dictionary of words (String[] d). One word per line.
-Constraints
-1 <= s <= 7
-1 <= n <= 100
-1<= d[i].length() <= 7
-you should assume all characters are lowercase a-z.
-Output Format
-Output the complete list of words found in one line with each word separated by a space.
-Sample Input 0
-cfhat
-10
-a
-at
-bat
-cat
-hat
-kite
-lion
-that
-yellow
-zebra
-Sample Output 0
-a at cat hat
-Explanation 0
-the four words that are output all appear in our 10 word dictionary.
-Note: you can create other words, but because they are not in our dictionary they are not added to the list - e.g. chat
-Also note: the words are output in lexigraphical order - in the same order as they would appear in a dictionary. You may want to generate a list and sort it before displaying.
-Also note: the number of letters we have to work with is small enough that all our promising decision needs to do is make sure we don't repeat the same letter more than once.
-An additional promising check could be whether or not any words in our dictionary begin with the word we are generating.
-*/
+
 
 import java.io.*;
 import java.util.*;
